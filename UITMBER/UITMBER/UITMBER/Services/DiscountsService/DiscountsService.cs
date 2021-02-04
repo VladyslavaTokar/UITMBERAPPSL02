3967f427637caca1797ed2e2bb63bc6b22fcf31a
w60083 <w60083@student.wsiz.rzeszow.pl>
2021-02-04 21:36:44 +0100

    DiscountsService

---
+++ UITMBER/UITMBER/UITMBER/Services/DiscountsService/DiscountsService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Services.Request;
+using Xamarin.Forms;
+
+namespace UITMBER.Services.DiscountsService
+{
+    class DiscountsService : IDiscountsService
+    {
+        private IRequestService _requestService => DependencyService.Get<IRequestService>();
+        public async Task<bool> AddToOrder(string code, long idorder)
+        {
+            var url = $"{Settings.SERVER_ENDPOINT}/Discounts/AddToOrder";
+            await _requestService.PostAsync<long>(url, idorder);
+
+            return true;
+        }
+
+        public async Task<IActionResult> Verify(string code)
+        {
+            var url = $"{Settings.SERVER_ENDPOINT}/Discounts/Verify";
+            var data = await _requestService.GetAsync<IActionResult>(url);
+
+            return data;
+        }
+    }   
+}
---
+++ UITMBER/UITMBER/UITMBER/Services/DiscountsService/IDiscountsService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITMBER.Services.DiscountsService
+{
+    public interface IDiscountsService
+    {
+       
+        Task<IActionResult> Verify(string code);
+        Task<bool> AddToOrder(string code, long idorder);
+
+
+    }
+}
